5db404a7c2a1c334855af0245adb9f471a0944db
amishel <amishel@gmail.com>
2024-08-10 19:49:51 +0000

    comments

--- example-app/connection-check-flow/test_connection.py
+++ example-app/connection-check-flow/test_connection.py
@@ -1,6 +1,7 @@
 from promptflow.core import tool
 from test_tools.tools.test_connection import TestConnection
 
+#this script tool works as expected, with the same connection type from the tools package
 @tool
 def validate(connection: TestConnection) -> bool: 
     return len(connection.test_key)>0 and len(connection.test_secret)>0
